b6790cec4ea0ce4fede728375c873dafb8fbcfca
Amr Hassan <amrmhassanwork@gmail.com>
2024-07-22 09:20:38 +0300

    aa

--- lib/features/restaurant/data/datasources/logging_datasource.dart
+++ lib/features/restaurant/data/datasources/logging_datasource.dart
@@ -3,12 +3,16 @@ import 'package:mongez_shared_code/constants/collections.dart';
 import 'package:mongez_shared_code/mongez_shared_code.dart';
 
 class LoggingDatasource {
-  Future<void> logOrderAction(
-    OrderModel model,
-    OrderLoggingAction action,
-  ) async {
+  Future<void> logOrderAction({
+    required String orderId,
+    required OrderLoggingAction action,
+    String? riderId,
+    String? restaurantId,
+  }) async {
     OrderLogging logging = OrderLogging(
-      orderId: model.id,
+      orderId: orderId,
+      restaurantId: restaurantId,
+      riderId: riderId,
       action: action,
       loggedAt: DateTime.now(),
     );
